ef5f6d8a5da79d2c53a45715c1291f5cc7f6177f
deadlybutter <thedeadlybutter@gmail.com>
2017-01-27 13:39:32 -0500

    Copy updates

--- app/js/components/Homepage.js
+++ app/js/components/Homepage.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Gallery from './Gallery';
 
+const header = "we're building bridges, not walls";
+const subheader = "So much hate speech lives online in posts and comments about refugees and immigrants. DoSomething.org members chose to show solidarity with those facing increasing levels of discrimination.";
+
 class Home extends Component {
   render() {
     return (
-      <Gallery />
+      <div>
+        <header className="wrapper">
+          <h1>{header}</h1>
+          <p>{subheader}</p>
+        </header>
+        <Gallery />
+      </div>
     );
   }
 }
--- app/js/components/Page.js
+++ app/js/components/Page.js
@@ -5,9 +5,6 @@ class Page extends Component {
   render() {
     return (
       <div>
-        <header className="wrapper">
-          <h1>this is our big beautiful wall</h1>
-        </header>
         {this.props.children}
         <footer className="wrapper">
           <p>&copy; 2017 DoSomething.org</p>
