9c96db2a2019f590c3494711f9e332a2c14d696c
deadlybutter <thedeadlybutter@gmail.com>
2017-01-28 18:09:17 -0500

    Style updates

--- app/js/components/Homepage.js
+++ app/js/components/Homepage.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import Gallery from './Gallery';
 
-const header = "we're building bridges, not walls";
+const header = [
+  "we're building bridges,",
+  "not walls",
+];
+
 const subheader = "So much hate speech lives online in posts and comments about refugees and immigrants. DoSomething.org members chose to show solidarity with those facing increasing levels of discrimination.";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <header className="wrapper">
-          <h1>{header}</h1>
-          <p>{subheader}</p>
+        <header className="container">
+          <div className="wrapper">
+            <h1>{header[0]}</h1>
+            <h1>{header[1]}</h1>
+            <p>{subheader}</p>
+          </div>
         </header>
         <Gallery />
       </div>
--- app/js/components/Page.js
+++ app/js/components/Page.js
@@ -5,6 +5,9 @@ class Page extends Component {
   render() {
     return (
       <div>
+        <a className="logo" href="http://www.dosomething.org">
+          <img src="/images/logo.png" />
+        </a>
         {this.props.children}
         <footer className="wrapper">
           <p>&copy; 2017 DoSomething.org</p>
