8aea1449a0a232e72a421ccb64f63bd420fefeb1
deadlybutter <thedeadlybutter@gmail.com>
2017-01-29 14:30:20 -0500

    Changes scroll behavior

--- app/js/components/Gallery.js
+++ app/js/components/Gallery.js
@@ -4,15 +4,26 @@ import 'whatwg-fetch';
 import Reportback from './Reportback';
 import Invite from './Invite';
 
+function reachedBottom() {
+  const buffer = 120;
+  const currentScroll = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
+  const pageHeight = document.body.offsetHeight;
+  const clientHeight = document.documentElement.clientHeight;
+
+  return pageHeight < currentScroll + clientHeight;
+}
+
 class Gallery extends Component {
   constructor(props) {
     super(props);
 
     this.fetchReportbacks = this.fetchReportbacks.bind(this);
+    this.bottomCheck = this.bottomCheck.bind(this);
 
     this.state = {
       reportbacks: [],
       page: 1,
+      end: false,
     }
   }
 
@@ -21,28 +32,37 @@ class Gallery extends Component {
     .then(res => res.json())
     .then(res => res.data)
     .then((reportbacks) => {
+      const end = reportbacks.length === 0;
+
       this.setState({
         reportbacks: [...this.state.reportbacks, ...reportbacks],
         page: this.state.page + 1,
+        end,
       });
     })
   }
 
+  bottomCheck() {
+    if (this.state.end) return;
+    if (reachedBottom()) {
+      this.fetchReportbacks();
+      setTimeout(this.bottomCheck, 1000);
+    } else {
+      setTimeout(this.bottomCheck, 100);
+    }
+  }
+
   componentDidMount() {
     this.fetchReportbacks();
-
-    window.onscroll = (ev) => {
-      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        this.fetchReportbacks();
-      }
-    };
+    setTimeout(this.bottomCheck, 100);
   }
 
   render() {
     return (
       <div className="container gallery">
+        <Onboarding />
         {this.state.reportbacks.map((rb, index) => {
-          if (index > 0 && index % 24 === 0) return <Invite />
+          if (index > 0 && index % 20 === 0) return <Invite />
           return <Reportback key={index} data={rb} />
         })}
       </div>
